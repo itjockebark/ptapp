52ae8a13b70b942c23015da977473203d20a8108
itjockebark <jockebark@msn.com>
2023-07-10 02:50:56 +0200

    Adds a PasswordConfig class and manipulates PasswordEncoder method
    Encrypts the password of a User

--- src/main/java/itjockebark/ptapp/service/UserService.java
+++ src/main/java/itjockebark/ptapp/service/UserService.java
@@ -4,24 +4,36 @@ import itjockebark.ptapp.dao.UserDAO;
 import itjockebark.ptapp.model.UserRole;
 import itjockebark.ptapp.model.dto.UserRegisterDTO;
 import itjockebark.ptapp.model.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
 
     public final String rolePrefix = "USER";
 
     private final UserDAO userDAO;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserDAO userDAO) {
+    public UserService(UserDAO userDAO, PasswordEncoder passwordEncoder) {
         this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 
     public void registerUser(UserRegisterDTO dto) {
         User user = new User();
 
         user.setEmail(dto.getEmail());
-        user.setPassword(dto.getPassword());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
         user.setBirthdate(dto.getBirthdate());
